f0d8e0bbedf21fed2bb9eca1c9b16a728e39eda3
Mahdi Fazel <uinermf@gmail.com>
2024-05-16 16:57:56 +0200

    secound rails watch list  Challenge

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,31 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  root "movie#index"
 
+  # resources :movies do
+  #   # Nested resources for bookmarks
+  #   resources :bookmarks, only: [:create]
+  # end
+
+  resources :lists, except: [:edit, :update] do
+    # Nested resources for bookmarks
+    resources :bookmarks, only: [:index, :new, :create]
+
+  end
+  resources :bookmarks, only: :destroy
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+
+
+# return RoutesSet.draw do
+#   get    '/restaurants',          to: 'restaurants#index'
+#   post   '/restaurants',          to: 'restaurants#create'
+#   get    '/restaurants/new',      to: 'restaurants#new'
+#   get    '/restaurants/:id/edit', to: 'restaurants#edit'
+#   get    '/restaurants/:id',      to: 'restaurants#show'
+#   patch  '/restaurants/:id',      to: 'restaurants#update'
+#   delete '/restaurants/:id',      to: 'restaurants#destroy'
+# end
